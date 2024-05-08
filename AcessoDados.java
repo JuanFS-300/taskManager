029cec438815787ca60599faf8d2ab2f13bd62ac
JuanFS-300 <66486156+JuanFS-300@users.noreply.github.com>
2024-05-08 20:11:38 -0300

    Update AcessoDados.java

--- AcessoDados.java
+++ AcessoDados.java
@@ -10,42 +10,57 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AcessoDados {
-    public static List<Funcao> leArquivo(String filePath){
+    public static List<Atividades> leArquivo(String filePath){
+
+    	List<Atividades> atividades = new ArrayList<>();
 
         try ( InputStream is = new FileInputStream(filePath);
                 InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
                 BufferedReader br = new BufferedReader(isr);
             ){
             String linha;
-            int i=0;
             while((linha = br.readLine()) != null){
 
-                System.out.println("linha " + i++);
+               
 
                 String[] palavras = linha.split(",");
 
                 for(String p: palavras){
                     System.out.println("palavra: " + p);
                 }
+                String nome = palavras[1];
+                
+                 String categoria = palavras[4];
+                
+                 String descricao = palavras[2];;
+                 //int duracao =Integer.parseInt(palavras[3]);;
+                 String prioridade = palavras[5];;
+                 
+                 int idAtividade = 1;
+
+                Atividades atividade = new Atividades(nome,descricao,prioridade,idAtividade, "",categoria, idAtividade);
+                atividades.add(atividade);
+                
 
             }
 
         }catch(IOException e){
             e.printStackTrace();
         }
-		return null;
+		return atividades;
     }
     
-    public static void saveArquivo(List<Funcao> funcoes, String filePath){
+    public static void saveArquivo(List<Atividades> funcoes, String filePath){
 
         try( OutputStream os = new FileOutputStream(filePath);
                 OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
                 PrintWriter pw = new PrintWriter(osw, true);
                 ){
-            for(Funcao linha: funcoes){
+            for(Atividades linha: funcoes){
                 pw.println(linha);
             }
 
