9dab4fe284f06c831e83a81be50730fab5bdc315
elissama290 <126925492+elissama290@users.noreply.github.com>
2024-05-08 21:27:10 -0300

    Update Funcao.java

--- Funcao.java
+++ Funcao.java
@@ -1,18 +1,27 @@
 package taskManager;
-//import java.util.ArrayList;
-import java.util.List;
 
-public class Funcao {
-	 
-	            //funcao
-			    public static Atividades buscarAtividade(List<Atividades> listaAtividades, int idAtividade) {
-			        for (Atividades atividade : listaAtividades) {
-			            if (atividade.getIdAtividade() == idAtividade ) {
-			                return atividade;
-			            }
-			        }
-			        return null; // Retornar null se a atividade não for encontrada
-			    }
-			
-	 }
+	//import java.util.ArrayList;
+	import java.util.List;
+
+	public class Funcao {
+		 
+		            //funcao
+				    public static Atividades buscarAtividade(List<Atividades> listaAtividades, int idAtividade) {
+				        for (Atividades atividade : listaAtividades) {
+				            if (atividade.getIdAtividade() == idAtividade ) {
+				                return atividade;
+				            }
+				        }
+				        return null; // Retornar null se a atividade não for encontrada
+				    }
+				    
+				    public static void adicionatarefa(List<Atividades> listaAtividades,String categoria, String nome, String descricao, int duracao, int id) {		
+						listaAtividades.add(new Atividades (categoria,nome, descricao, duracao, id));		
+					}
+
+				
+		 }
+
+
+
 
