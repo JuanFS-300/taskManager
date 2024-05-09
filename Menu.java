7c0f6ba1fb6a586e45727221efe2e61d0ae1b659
JuanFS-300 <66486156+JuanFS-300@users.noreply.github.com>
2024-05-09 19:19:59 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,69 +3,73 @@ package taskManager;
 import java.util.Scanner;
 import java.util.List;
 
-
 public class Menu {
+	private static Scanner le;
+
 	public static void menu() {
 		System.out.println("--------------APLICATIVO DE GERENCIAMENTO DE TAREFAS---------------\n");
 		System.out.println("1. Lista Tarefas do dia a dia");
-		System.out.println("2. Lista Tarefas por categoria");
+		System.out.println("2. Buscar tarefa por ID");
 		System.out.println("3. Adiciona Tarefas");
 		System.out.println("4. Exclui Tarefas");
 		System.out.println("5. Sair");
-		System.out.println("Digite uma das opções:");	
+		System.out.println("Digite uma das opções:");
 	}
-	
-	public static void main (String args[]) {
+
+	public static void main(String args[]) {
 		int opcao = 0;
-		
-		Scanner le = new Scanner(System.in);
+
+		le = new Scanner(System.in);
 		menu();
-		
-		opcao= le.nextInt();
-		
+
+		opcao = le.nextInt();
+
 		switch (opcao) {
 		case 1:
-			listadodia();
+			// listadodia();
 			break;
 		case 2:
-			listatarefa();
+			List<Atividades> atividades = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			System.out.println("Insira o id para buscar atividade:");
+			int idAtividade = le.nextInt();
+			
+
+			Atividades atividadeEncontrada = Funcao.buscarAtividade(atividades, idAtividade);
+
+			if (atividadeEncontrada != null) {
+
+				System.out.println("Atividade encontrada: " + atividadeEncontrada.toString());
+			} else {
+
+				System.out.println("Atividade com o ID " + idAtividade + " não encontrada.");
+			}
+
 			break;
 		case 3:
-		
-			System.out.println("Adicionar Categoria:");
-			String categoria = le.nextLine();
+			List<Atividades> listaAtividades = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			
 			
 			System.out.println("Adicionar Nome:");
-			String nome = le.nextLine();
-			
-			System.out.println("Adicionar Descriçao da Tarefa:");
-			String descricao = le.nextLine();
-			
+			String nome = le.next();
+			System.out.println("Adicionar Descricao:");
+			String descricao = le.next();
+			System.out.println("Adicionar Categoria:");
+			String categoria = le.next();
 			System.out.println("Adicionar Duração:");
-			Integer duracao = le.nextInt();
-			
+			String duracao = le.next();
 			System.out.println("Adicionar Id:");
-			Integer id = le.nextInt();
+			int id = le.nextInt();
+
 			
-			List<Atividades> listaAtividades = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");;
-			Funcao.adicionatarefa(listaAtividades,categoria, nome, descricao, duracao, id);
+			Funcao.adicionatarefa(listaAtividades, categoria, nome, descricao, duracao, id);
+			
 			break;
-		case 4:
-			excluitarefa();
-			break;
-		case 5:
-			sair();
-			break;
+		
 		default:
 			System.out.println("Opcão invalida, tente novamente!.");
-		
+
 		}
 	}
 
-	private static void sair() {
-		
-	}
-
-	private static void excluitarefa() {
-		
-	}
+	
+}
