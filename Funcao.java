005bb2c6da2f2e997b2d81ff9d08eb6e92e6ab03
JuanFS-300 <66486156+JuanFS-300@users.noreply.github.com>
2024-05-09 19:19:22 -0300

    Update Funcao.java

--- Funcao.java
+++ Funcao.java
@@ -1,27 +1,28 @@
 package taskManager;
 
-	//import java.util.ArrayList;
-	import java.util.List;
+//import java.util.ArrayList;
+import java.util.List;
 
-	public class Funcao {
-		 
-		            //funcao
-				    public static Atividades buscarAtividade(List<Atividades> listaAtividades, int idAtividade) {
-				        for (Atividades atividade : listaAtividades) {
-				            if (atividade.getIdAtividade() == idAtividade ) {
-				                return atividade;
-				            }
-				        }
-				        return null; // Retornar null se a atividade não for encontrada
-				    }
-				    
-				    public static void adicionatarefa(List<Atividades> listaAtividades,String categoria, String nome, String descricao, int duracao, int id) {		
-						listaAtividades.add(new Atividades (categoria,nome, descricao, duracao, id));		
-					}
+public class Funcao {
 
-				
-		 }
+	// funcao
+	public static Atividades buscarAtividade(List<Atividades> listaAtividades, int idAtividade) {
+		
+		for (Atividades atividade : listaAtividades) {
+	
+			if (atividade.getIdAtividade() == idAtividade) {
+				return atividade;
+			}
+		}
+		return null;
 
+	}
 
+	public static void adicionatarefa(List<Atividades> listaAtividades, String categoria, String nome, String descricao,
+			String duracao, int id) {
+		listaAtividades.add(new Atividades(categoria, nome, descricao, duracao, id));
+		 AcessoDados.saveArquivo(listaAtividades, "C:\\\\Users\\\\juanf\\\\Downloads\\\\taskManager.csv");
 
+	}
 
+}
