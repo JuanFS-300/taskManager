0d83959e2390b5842a35f96fac56e9738ac39908
JuanFS-300 <66486156+JuanFS-300@users.noreply.github.com>
2024-05-13 20:23:45 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,68 +1,96 @@
 package taskManager;
 
 import java.util.Scanner;
+
+import taskManager.AcessoDados.ListagemFinal;
+
 import java.util.List;
 
 public class Menu {
 	private static Scanner le;
 
 	public static void menu() {
-		System.out.println("--------------APLICATIVO DE GERENCIAMENTO DE TAREFAS---------------\n");
-		System.out.println("1. Lista Tarefas do dia a dia");
-		System.out.println("2. Buscar tarefa por ID");
+		System.out.println("\n\n--------------APLICATIVO DE GERENCIAMENTO DE TAREFAS---------------\n");
+		System.out.println("1. Listar tarefas por dia");
+		System.out.println("2. Buscar tarefa por nome");
 		System.out.println("3. Adiciona Tarefas");
-		System.out.println("4. Exclui Tarefas");
+		System.out.println("4. Listar todas as tarefas");
 		System.out.println("5. Sair");
-		System.out.println("Digite uma das opções:");
-	}
 
-	public static void main(String args[]) {
+		System.out.println("Digite uma das opções:");
+	
 		int opcao = 0;
 
 		le = new Scanner(System.in);
-		menu();
+		//menu();
 
 		opcao = le.nextInt();
 
 		switch (opcao) {
 		case 1:
-			// listadodia();
+			List<Atividades> listagemAtividade = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			List<TarefasData> listagemData = AcessoDados.leArquivoData("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			
+			System.out.println("Insira o categoria para buscar atividade:");
+			String categoria = le.next();
+			
+
+			List<ListagemFinal> listaPorCategoria = Funcao.listarPorCategoria(listagemAtividade,listagemData, categoria);
+			
+			if(listaPorCategoria.isEmpty() == true) {
+				System.out.println("Não foram encontradas atividades para essa categoria!");
+			}else {
+				 Funcao.showAllAtividades(listaPorCategoria);
+			}
+			
+			confirmacaoSaida();
 			break;
 		case 2:
 			List<Atividades> atividades = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
-			System.out.println("Insira o id para buscar atividade:");
-			int idAtividade = le.nextInt();
+			List<TarefasData> listaData = AcessoDados.leArquivoData("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			
+			System.out.println("Insira o nome para buscar atividade:");
+			String nome = le.next();
 			
 
-			Atividades atividadeEncontrada = Funcao.buscarAtividade(atividades, idAtividade);
+			ListagemFinal atividadeEncontrada = Funcao.buscarAtividade(atividades,listaData, nome);
 
 			if (atividadeEncontrada != null) {
 
-				System.out.println("Atividade encontrada: " + atividadeEncontrada.toString());
+				System.out.println("Atividade encontrada: ");
+				System.out.println("Nome da Atividade: " + atividadeEncontrada.getAtividade().getNome());
+				System.out.println("Categoria: " + atividadeEncontrada.getAtividade().getCategoria() + " | Descrição: " + atividadeEncontrada.getAtividade().getDescricao());
+				System.out.println("Data: " + atividadeEncontrada.getData().getDataDia() + "/" + atividadeEncontrada.getData().getDataMes() + "/" +atividadeEncontrada.getData().getDataAno() + "\n");
+				
+				
 			} else {
 
-				System.out.println("Atividade com o ID " + idAtividade + " não encontrada.");
+				System.out.println("Atividade com o nome'" + nome + "' não encontrada.");
 			}
-
+			confirmacaoSaida();
 			break;
 		case 3:
 			List<Atividades> listaAtividades = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			List<TarefasData> listaDatas = AcessoDados.leArquivoData("C:\\Users\\juanf\\Downloads\\taskManager.csv");
 			
 			
-			System.out.println("Adicionar Nome:");
-			String nome = le.next();
-			System.out.println("Adicionar Descricao:");
-			String descricao = le.next();
-			System.out.println("Adicionar Categoria:");
-			String categoria = le.next();
-			System.out.println("Adicionar Duração:");
-			String duracao = le.next();
-			System.out.println("Adicionar Id:");
-			int id = le.nextInt();
+		 Atividades atividade = Funcao.getNewAtividadeFromUser();
+		 TarefasData data = Funcao.getNewDataFromUser();
 
 			
-			Funcao.adicionatarefa(listaAtividades, categoria, nome, descricao, duracao, id);
-			
+			Funcao.adicionatarefa(listaAtividades, atividade, listaDatas, data);
+			confirmacaoSaida();
+			break;
+		case 4:
+			List<Atividades> listaAtividadesCompleta = AcessoDados.leArquivo("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			List<TarefasData> datas = AcessoDados.leArquivoData("C:\\Users\\juanf\\Downloads\\taskManager.csv");
+			List<ListagemFinal> listagemConcatenada = AcessoDados.concatenarLista(listaAtividadesCompleta,datas);
+		    Funcao.showAllAtividades(listagemConcatenada);
+		    confirmacaoSaida();
+		    
+			break;
+		case 5:
+			System.out.println("\n\n--------------APLICATIVO ENCERRADO---------------\n");
 			break;
 		
 		default:
@@ -70,6 +98,25 @@ public class Menu {
 
 		}
 	}
-
 	
+public static void confirmacaoSaida() {
+	System.out.println("Deseja encerrar o aplicativo?");
+	System.out.println("1 - Para retornar ao menu");
+	System.out.println("2 - Para Encerrar aplicativo");
+	int prosseguir = le.nextInt();
+	if(prosseguir == 1) {
+		 menu();
+	} else if(prosseguir == 2) {
+		System.out.println("\n\n--------------APLICATIVO ENCERRADO---------------\n");
+		
+	} else {
+		System.out.println("Opcão invalida, tente novamente!.");
+		confirmacaoSaida();
+	}
+}
+
+	public static void main(String args[]) {
+		
+
+	}
 }
